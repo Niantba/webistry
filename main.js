f1d12a705933bd23feb5812de4152759aec16df1
Audrey Baniant <audrey.baniant@gmail.com>
2023-09-14 13:53:23 -0400

    css completed, validation to be done

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // You can remove this line when you're ready.
 console.log('Good luck!');
+
+function validateForm() {
+  const name = document.forms["webistryForm"]["name"].value;
+  const email = document.forms["webistryForm"]["email"].value;
+
+  if (name == "")   {
+    alert("Name must be filled out");
+    return false;
+  } else if (email == "")   {
+    alert("Email must be filled out");
+    return false;
+  }
+}
